174bfc40b4b1d2415eaf970ec1ff54cdcc53fe3f
rocksnow1942 <rocksnow1942@gmail.com>
2023-12-22 20:51:55 -0800

    a

--- main.py
+++ main.py
@@ -1,24 +1,23 @@
 #!/usr/bin/env python
 import configparser
 from pathlib import Path
-from sonyA7Cimport import getImageVides,organizeFile
-from videoshare import compressVideos,uploadVideos
 
+from sonyA7Cimport import getImageVides, organizeFile
+from videoshare import compressVideos, uploadVideos
 
 config = configparser.ConfigParser()
-config.read(Path(__file__).parent / 'config.ini')
-print(config.sections())
-
+config.read(Path(__file__).parent / "config.ini")
 
 
 if __name__ == "__main__":
-    images, videos = getImageVides(config['CAMERA_FOLDER']['Images'],config['CAMERA_FOLDER']['Videos'] )
-    newImages = organizeFile(images, config['SAVE_FOLDER']['Images'])
-    newVideos = organizeFile(videos, config['SAVE_FOLDER']['Videos'])
+    images, videos = getImageVides(
+        config["CAMERA_FOLDER"]["Images"], config["CAMERA_FOLDER"]["Videos"]
+    )
+    newImages = organizeFile(images, config["SAVE_FOLDER"]["Images"])
+    newVideos = organizeFile(videos, config["SAVE_FOLDER"]["Videos"])
     # compressed = compressVideos(newVideos, mode="iphone")
-    
+
     # uploadVideos(compressed)
-    
+
     # also compress for storage. ~ 50% the size.
     compressVideos(newVideos, mode="storage")
-    
